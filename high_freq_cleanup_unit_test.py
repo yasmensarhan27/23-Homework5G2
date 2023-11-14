432434a9ca59de430afab9059085b5978be7dfd2
Pranjal Srivastava <143828394+Pranjal-Srivastava-2023@users.noreply.github.com>
2023-11-14 16:56:05 -0500

    Create high_freq_cleanup_unit_test.py

---
+++ high_freq_cleanup_unit_test.py
@@ -0,0 +1,68 @@
+import unittest
+from unittest.mock import patch, mock_open
+import pandas as pd
+import numpy as np
+
+class TestCO2Analysis(unittest.TestCase):
+    def setUp(self):
+        # Sample data for testing
+        self.sample_url = 'https://gml.noaa.gov/aftp/data/trace_gases/co2/flask/surface/txt/co2_avi_surface-flask_1_ccgg_month.txt'
+        self.sample_data = """site year month value
+                              A    2022  1     400.0
+                              A    2022  2     410.0
+                              A    2022  3     420.0
+                              B    2022  1     390.0
+                              B    2022  2     400.0
+                              B    2022  3     410.0"""
+
+    @patch('pandas.read_csv', side_effect=pd.read_csv)
+    def test_load_data(self, mock_read_csv):
+        # Test if data is loaded correctly
+        mock_read_csv.return_value = pd.read_csv(pd.compat.StringIO(self.sample_data), delimiter="\s+", skiprows=54, names=['site', 'year', 'month', 'value'])
+
+        df = load_data(self.sample_url)
+
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertEqual(len(df), 6)
+        self.assertListEqual(list(df.columns), ['site', 'year', 'month', 'value'])
+        self.assertListEqual(list(df['value']), [400.0, 410.0, 420.0, 390.0, 400.0, 410.0])
+
+    @patch('pandas.read_csv', side_effect=pd.read_csv)
+    def test_fft_and_frequency_analysis(self, mock_read_csv):
+        # Test FFT and frequency analysis
+        mock_read_csv.return_value = pd.read_csv(pd.compat.StringIO(self.sample_data), delimiter="\s+", skiprows=54, names=['site', 'year', 'month', 'value'])
+        df = load_data(self.sample_url)
+        fft_result_raw = np.fft.fft(df['value'])
+        time_step = 1
+        frequencies_per_month_raw = np.fft.fftfreq(len(fft_result_raw), d=time_step)
+
+        # Add assertions for FFT and frequency analysis
+        self.assertIsInstance(fft_result_raw, np.ndarray)
+        self.assertIsInstance(frequencies_per_month_raw, np.ndarray)
+        self.assertEqual(len(fft_result_raw), len(frequencies_per_month_raw))
+
+    def test_high_frequency_component_removal(self):
+        # Test high-frequency component removal
+        fft_result_raw = np.fft.fft([1, 2, 3, 4, 5])
+        frequencies_per_month_raw = np.fft.fftfreq(len(fft_result_raw), d=1)
+        threshold = 0.01
+        fft_result_raw[np.abs(frequencies_per_month_raw) > threshold] = 0
+
+        # Add assertions for high-frequency component removal
+        self.assertTrue(np.all(fft_result_raw[np.abs(frequencies_per_month_raw) > threshold] == 0))
+
+    def test_inverse_fft_and_cleaned_data_plotting(self):
+        # Test inverse FFT and cleaned data plotting
+        fft_result_raw = np.fft.fft([1, 2, 3, 4, 5])
+        time_step = 1
+        frequencies_per_month_raw = np.fft.fftfreq(len(fft_result_raw), d=time_step)
+        threshold = 0.01
+        fft_result_raw[np.abs(frequencies_per_month_raw) > threshold] = 0
+        cleaned_data_raw = np.fft.ifft(fft_result_raw)
+
+        # Add assertions for inverse FFT and cleaned data plotting
+        self.assertIsInstance(cleaned_data_raw, np.ndarray)
+        self.assertEqual(len(cleaned_data_raw), len(fft_result_raw))
+
+if __name__ == '__main__':
+    unittest.main()
