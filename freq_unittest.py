de827672288bb20f1b4762c0113948f9eab7072f
Yasmin Sarhan <38404107+yasmensarhan27@users.noreply.github.com>
2023-11-17 02:00:12 -0500

    Update freq_unittest.py

--- freq_unittest.py
+++ freq_unittest.py
@@ -3,56 +3,42 @@ import pandas as pd
 import numpy as np
 from unittest.mock import patch
 from peak_frequency import find_peak_frequency
-
 class FindPeakFrequencyTest(unittest.TestCase):
-
     def test_find_peak_frequency_with_valid_data(self):
         # Create a sample dataset
-        data = pd.DataFrame({'month': np.linspace(0, 1, 100),
-                     'value': np.sin(2 * np.pi * 10 * np.linspace(0, 1, 100))})
-
+        data = pd.DataFrame({'year': np.linspace(1990, 2000, 100)
+                             'month': np.linspace(0, 1, 100),
+                             'value': np.sin(2 * np.pi * 10 * np.linspace(0, 1, 100))})
         # Calculate the expected peak frequency
         expected_cycles_per_year = 3.0
-
         # Calculate the actual peak frequency
         actual_cycles_per_year = find_peak_frequency(data)
-
         # Assert that the actual peak frequency matches the expected peak frequency
         self.assertEqual(actual_cycles_per_year, expected_cycles_per_year)
-
     def test_find_peak_frequency_with_high_frequency_noise(self):
         # Create a sample dataset with high-frequency noise
         data = pd.DataFrame({'year': [2021, 2021, 2021],
                              'month': [1, 2, 3],
                              'value': [395.78, 396.15, 397.38 + np.random.normal(0, 0.1)]})
-
         # Calculate the expected peak frequency
         expected_cycles_per_year = 0.0
-
         # Calculate the actual peak frequency
         actual_cycles_per_year = find_peak_frequency(data)
-
         # Assert that the actual peak frequency matches the expected peak frequency
         self.assertEqual(actual_cycles_per_year, expected_cycles_per_year)
-
     @patch('numpy.fft.fftfreq')
     def test_find_peak_frequency_with_mocked_fftfreq(self, mock_fftfreq):
         # Mock the fftfreq function to return a specific set of frequencies
         mock_fftfreq.return_value = np.array([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])
-
         # Create a sample dataset
         data = pd.DataFrame({'year': [2021, 2021, 2021],
                              'month': [1, 2, 3],
                              'value': [395.78, 396.15, 397.38]})
-
         # Calculate the expected peak frequency
         expected_cycles_per_year = 0.2
-
         # Calculate the actual peak frequency
         actual_cycles_per_year = find_peak_frequency(data)
-
         # Assert that the actual peak frequency matches the expected peak frequency
         self.assertEqual(actual_cycles_per_year, expected_cycles_per_year)
-
 if __name__ == '__main__':
     unittest.main()
