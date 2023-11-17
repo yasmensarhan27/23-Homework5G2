944cc498a1c2716da39f27cec480e1fcb59f6764
Yasmin Sarhan <38404107+yasmensarhan27@users.noreply.github.com>
2023-11-17 01:56:48 -0500

    Update peak_frequency.py

--- peak_frequency.py
+++ peak_frequency.py
@@ -31,7 +31,7 @@ def find_peak_frequency(data):
     # Set a threshold to identify high-frequency components
     threshold = 0.01
     fft_result[np.abs(fft_freq) > threshold] = 0  # Zero out high-frequency components
-    if fft_freq_length != fft_result_length:
+    if len(fft_freq) != len(fft_result):
       raise ValueError("FFT frequency and result arrays must have the same length.")
     cleaned_data = np.fft.ifft(fft_result)
     # Find the frequency component with the highest magnitude
