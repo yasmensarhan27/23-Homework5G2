8e8eff7fae7a4048ccb3e32e872af35bf6cd4666
Yasmin Sarhan <38404107+yasmensarhan27@users.noreply.github.com>
2023-11-17 00:31:58 -0500

    Update peak_frequency.py

--- peak_frequency.py
+++ peak_frequency.py
@@ -1,15 +1,19 @@
+"""
+This code will perform:
+1- the fast fourier transform to the data
+3- take the output from the cleaned and filtered data
+(removes the first peak at zero to get some reasonable frequency of the data)
+2- choose the best peak for the frequency calculation
+"""
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
 def find_peak_frequency(data):
     """
     Function takes a pandas DataFrame `data` containing a column named 'value' to represent a time series of measurements. 
-
     Parameters:
     - data : pandas DataFrame
         Data containing date and values with date assigned in years and months.
-
     Returns:
     - cycles_per_year : float
         The estimated frequency in cycles per year.
@@ -18,7 +22,8 @@ def find_peak_frequency(data):
     # Perform a Fast Fourier Transform (FFT) on the data
     timestep = 1  # Calculate the time step (assuming a regular monthly sampling)
     fft_result = np.fft.fft(values)
-    fft_freq = np.fft.fftfreq(len(fft_result), d=timestep)  # Compute the corresponding frequencies in cycles per month
+    # Compute the corresponding frequencies in cycles per month
+    fft_freq = np.fft.fftfreq(len(fft_result), d=timestep)
     # Set a threshold to identify high-frequency components
     threshold = 0.01
     fft_result[np.abs(fft_freq) > threshold] = 0  # Zero out high-frequency components
@@ -29,7 +34,10 @@ def find_peak_frequency(data):
     # Convert the peak frequency to useful units
     cycles_per_year = peak_frequency * 12  # Assuming 12 months in a year
     return cycles_per_year
+    #import data from the chosen noaa station 
 data = pd.read_csv('https://gml.noaa.gov/aftp/data/trace_gases/co2/flask/surface/txt/co2_avi_surface-flask_1_ccgg_month.txt',
-                   delimiter='\s+', skiprows=54, names=['site', 'year', 'month', 'value'])
+                   delimiter="\s+", skiprows=54, names=['site', 'year', 'month', 'value'])
+#apply the find_frequency peak function to the data
 cycles_per_year = find_peak_frequency(data)
+# print the frequency in reasonable unit
 print("Peak Frequency (Cycles per year):", cycles_per_year)
