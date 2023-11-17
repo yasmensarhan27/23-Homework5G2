183aac1ef27322bd335c7595c2924e34cb19a29f
Yasmin Sarhan <38404107+yasmensarhan27@users.noreply.github.com>
2023-11-17 03:23:33 -0500

    Update freq_unittest.py

--- freq_unittest.py
+++ freq_unittest.py
@@ -1,10 +1,20 @@
+"""
+This code will test the main moduke peak_frequency
+1- creating a sample data set and check the frequency for a valid data
+2- Create a sample dataset with high-frequency noise and test the frequency
+3- Mock the fftfreq function to return a specific set of frequeies 
+"""
 import unittest
+from unittest.mock import patch
 import pandas as pd
 import numpy as np
-from unittest.mock import patch
 from peak_frequency import find_peak_frequency
 class FindPeakFrequencyTest(unittest.TestCase):
     def test_find_peak_frequency_with_valid_data(self):
+        """
+        test the sample data set
+        calculate the fequency and compare it to the expected one
+        """
         # Create a sample dataset
         data = pd.DataFrame({'year': np.linspace(1990, 2000, 100),
                              'month': np.linspace(0, 1, 100),
@@ -16,6 +26,10 @@ class FindPeakFrequencyTest(unittest.TestCase):
         # Assert that the actual peak frequency matches the expected peak frequency
         self.assertEqual(actual_cycles_per_year, expected_cycles_per_year)
     def test_find_peak_frequency_with_high_frequency_noise(self):
+        """
+        create sample datset with high noise
+        calculate the fequency and compare it to the expected one
+        """
         # Create a sample dataset with high-frequency noise
         data = pd.DataFrame({'year': [2021, 2021, 2021],
                              'month': [1, 2, 3],
@@ -28,6 +42,10 @@ class FindPeakFrequencyTest(unittest.TestCase):
         self.assertEqual(actual_cycles_per_year, expected_cycles_per_year)
     @patch('numpy.fft.fftfreq')
     def test_find_peak_frequency_with_mocked_fftfreq(self, mock_fftfreq):
+        """
+        mock the fftfreq and return the value in array
+        calculate the fequency and compare it to the expected one
+        """
         # Mock the fftfreq function to return a specific set of frequencies
         mock_fftfreq.return_value = np.array([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])
         # Create a sample dataset
